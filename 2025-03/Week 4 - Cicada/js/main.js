5844701f74d915174b0ef9ed1ab2c1faa331250c
nathanieljrodriguez <njr@nathanielrodriguez.me>
2025-03-27 08:38:52 -0400

    Change from Isometric Grid to Dynamic Background

--- 2025-03/Week 4 - Cicada/js/main.js
+++ 2025-03/Week 4 - Cicada/js/main.js
@@ -42,6 +42,88 @@ function generatePrimes(size)
 	}
 	
 	array = array.filter(element => (element !== -1));
-
+	
+	console.log(array);
+	
 	return array;
+}
+
+function gB(width, height)
+{
+	
+	document.querySelector(".grid").innerHTML = "";
+		
+	let primes = generatePrimes(width * height);
+	
+	for (let rows = 0; rows < height; ++rows)
+	{
+		let rowDiv = document.createElement("div");
+		
+		rowDiv.classList.add("col");
+		rowDiv.classList.add("col-" + rows);
+		
+		rowDiv.style.gridRow = rows;
+		
+		for (let cols = 0; cols < width; ++cols)
+		{
+			let gridValue = (width * rows) + cols;
+			
+			if (primes.includes(gridValue))
+			{
+				console.log(gridValue + " is prime. :)");
+				
+				let r = ((Math.random() * 100) + cols);
+				let g = ((Math.random() * 100) + cols);
+				let b = ((Math.random() * 100) + cols);
+				let a = ((Math.random()));
+				
+				
+				let colDiv = document.createElement("div");
+				colDiv.classList.add("row");
+				colDiv.classList.add("col-" + rows + "-row-" + cols);
+				colDiv.style.backgroundColor = "rgba(" + r + ", " + g + ", " + b + ", " + a + ")";
+				colDiv.innerText = gridValue;
+				
+				rowDiv.appendChild(colDiv);
+			}
+		}
+		
+		document.querySelector(".grid").appendChild(rowDiv);
+	}
+}
+
+function generateBackground(width, height)
+{
+	let primes = generatePrimes(width * height);
+	
+	for(let col = 0; col < width; ++col)
+	{
+		let colDiv = document.createElement("div");
+		colDiv.classList.add("col");
+		colDiv.classList.add("col-" + col);
+		for (let row = 0; row < height; ++row)
+		{
+			
+			if (primes.includes(((height) * row) + col))
+			{
+			let value = (height * row) + col;
+			console.log("col: " + col);
+			console.log("row: " + row);
+			console.log("value: " + value);
+				let r = ((Math.random() * 100) + row);
+				let g = ((Math.random() * 100) + row);
+				let b = ((Math.random() * 100) + row);
+				let a = ((Math.random()));
+				
+				
+				let rowDiv = document.createElement("div");
+				rowDiv.classList.add("row");
+				rowDiv.classList.add("col-" + col + "-row-" + row);
+				rowDiv.style.backgroundColor = "rgba(" + r + ", " + g + ", " + b + ", " + a + ")";
+				colDiv.appendChild(rowDiv);
+				rowDiv.innerText = value;
+			}
+		}
+		document.querySelector(".grid").appendChild(colDiv);
+	}
 }
